e82328ead9ad0bc042c5eabc9fcd82096d7072a4
Manuel <joseguns2@gmail.com>
2025-03-29 19:44:40 -0700

    Trabajando en la Experiencia

--- src/components/layout/Experiencia.jsx
+++ src/components/layout/Experiencia.jsx
@@ -2,7 +2,7 @@ import { BackgroundGradient } from "../background-gradient.jsx";
 
 export default function Experiencia() {
     return (
-        <section class="mt-16 grid grid-cols-2 gap-4 container mx-auto px-4">
+        <section class="mt-16 grid grid-cols-2 gap-x-4 gap-y-8 container mx-auto px-4">
             <BackgroundGradient className="rounded-[22px] p-4 sm:p-10 dark:bg-zinc-900 flex justify-center items-center">
                 <img src="/src/assets/Yopi.png" alt="yo" className="h-96" />
             </BackgroundGradient>
@@ -28,7 +28,14 @@ export default function Experiencia() {
             <div class="container mx-auto px-4 col-span-2">
                 <h2 class="text-3xl font-bold text-center mb-8">Experiencia</h2>
             </div>
-          <div></div>
+            <BackgroundGradient className="rounded-[22px] p-3 sm:p-8 dark:bg-zinc-900">
+                <a href="#" target="_blank" rel="noopener noreferrer">
+                    <img src="/src/assets/DoffyWeb.png" alt="DoffyWeb" className="border-2 border-zinc-700 rounded-lg" />
+                    <br />
+                    <p className="text-balance">DoffyWeb</p>
+
+                </a>
+            </BackgroundGradient>
         </section>
     );
 }
