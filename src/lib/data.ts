a6e495c654cdbf25d0816ac7edd261dc5ba8ee35
Manuel <joseguns2@gmail.com>
2025-03-31 00:47:27 -0700

    Update Experiencia

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -19,8 +19,36 @@ export const experiencia = [
     {
         nombre: "VigasApp",
         imagen: "/src/assets/VigasApp.png",
-        descripcion: "Sistema de inventario de vigas de madera",
+        descripcion: "Sistema Backend de inventario de vigas de madera",
         tecnologias: ['Django', 'PostgreSQL'],
         url: "https://vigasapp-production.up.railway.app/api/",
+    },
+    {
+        nombre: "Sistema de Gestion de Inventario",
+        imagen: "/src/assets/SGI.png",
+        descripcion: "Sistema Frontend de inventario de vigas de madera",
+        tecnologias: ['React', 'Tailwind'],
+        url: "https://inventory-beams.vercel.app/app",
+    },
+    {
+        nombre: "Terminal de Tunja",
+        imagen: "/src/assets/TerminalTunja.png",
+        descripcion: "Landing Page de la pagina web de la terminal de Tunja",
+        tecnologias: ['React', 'Sass'],
+        url: "https://test-terminal-tunja.vercel.app/",
+    },
+    {
+        nombre: "BePelican",
+        imagen: "/src/assets/BePelican.png",
+        descripcion: "Landing Page de la pagina web de Be Pelican",
+        tecnologias: ['HTML5', 'CSS3', 'JavaScript'],
+        url: "https://landing-bepelican.vercel.app/",
+    },
+    {
+        nombre: "LoremDiv",
+        imagen: "/src/assets/LoremDiv.png",
+        descripcion: "Antigua pagina personal de FreeLancer de mi hermano",
+        tecnologias: ['React', 'Bootstrap','Sass'],
+        url: "https://lorem-div-page.vercel.app/",
     },
 ]
