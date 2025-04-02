d97fdedeb62a548b1b446df447be061388396f8c
Manuel <joseguns2@gmail.com>
2025-04-02 01:07:53 -0700

    Update

--- src/i18n/lang.ts
+++ src/i18n/lang.ts
@@ -5,11 +5,17 @@ export const listLang: Record<string, string> = {
 
 export const defaultLang: string = 'es';
 
-export const ui: Record<string, Record<string, string>> = {
+export const ui: Record<string, Record<string, string | Record<string, string>>> = {
     es: {
         nav: 'Bienvenido a mi portafolio',
+        hero: {
+            title: 'Desarrollador de software',
+        }
     },
     en: {
         nav: 'Welcome to my portfolio',
+        hero: {
+            title: 'Software Developer',
+        }
     }
 } as const;
