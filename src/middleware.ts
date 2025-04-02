b424d198a9033ee7438706ea930ba0d2d5485663
Manuel <joseguns2@gmail.com>
2025-04-02 07:42:42 -0700

    Redirect

---
+++ src/middleware.ts
@@ -0,0 +1,32 @@
+import { defineMiddleware } from "astro:middleware";
+import { defaultLang, listLang } from "./i18n/lang";
+
+export const onRequest = defineMiddleware((context, next) => {
+  const { pathname, origin } = new URL(context.request.url);
+
+  // Si ya estamos en una ruta con idioma, continuamos
+  if (pathname.match(/^\/(es|en)\/?/)) {
+    return next();
+  }
+
+  // Obtener el idioma preferido del navegador
+  const acceptLanguage = context.request.headers.get("accept-language");
+  let preferredLang = defaultLang;
+
+  if (acceptLanguage) {
+    const browserLangs = acceptLanguage.split(",").map(lang => lang.split(";")[0].trim());
+    const supportedLangs = Object.keys(listLang);
+    
+    // Encontrar el primer idioma soportado
+    const matchedLang = browserLangs.find(lang => 
+      supportedLangs.some(supported => lang.startsWith(supported))
+    );
+
+    if (matchedLang) {
+      preferredLang = matchedLang.substring(0, 2);
+    }
+  }
+
+  // Redirigir a la ruta con el idioma
+  return Response.redirect(new URL(`/${preferredLang}${pathname}`, origin));
+});
